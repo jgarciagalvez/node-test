40c513b3b607b98348ab356b270775138970b637
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-14 12:47:35 +0700

    Install lodash and axios packages

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import { timezone } from "./constants.js";
 import { pi, tau} from "./constants.js";
 import { calcCircleArea } from "./math/functions.js";
+import lodash from 'lodash'
+import axios from 'axios'
 
 
-console.log(timezone)
-console.log(tau)
-console.log(pi)
+// console.log(timezone)
+// console.log(tau)
+// console.log(pi)
 
-console.log(calcCircleArea(10, pi)) // 314
-console.log(calcCircleArea(5, pi)) // 78.5
+// console.log(calcCircleArea(10, pi)) // 314
+// console.log(calcCircleArea(5, pi)) // 78.5
+
+let a = [1, [2, [3, [4]], 5]]
+
+// console.log(lodash.flatten(a))
+
+let b = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+
+console.log(b.data)
+
