f15c3ee77ca6f171516ee15e66764de0e44f45e5
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-14 12:01:39 +0700

    Create pi and tau constants

--- constants.js
+++ constants.js
@@ -1,1 +1,5 @@
 export const timezone = 'Indochina'
+
+export const pi = 3.14
+export const tau = 6.28
+
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import { timezone } from "./constants.js";
+import { pi, tau} from "./constants.js";
+
 
 console.log(timezone)
+console.log(tau)
+console.log(pi)
